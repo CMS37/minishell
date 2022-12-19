e2118b9902de82cfdddfd4faec773e05b21a2513
Younganswer <dudtj1999@gmail.com>
2022-12-19 14:23:55 +0900

    Default minishell file initialized

---
+++ srcs/minishell.c
@@ -0,0 +1,10 @@
+#include "../incs/minishell.h"
+#include "../libs/ft_printf/includes/ft_printf.h"
+
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+	ft_printf("Hello World!\n");
+	return (0);
+}
