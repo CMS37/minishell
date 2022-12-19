cb3c6dde182bff61f97ccd43f2aae93748a977f8
Younganswer <dudtj1999@gmail.com>
2022-12-19 14:45:06 +0900

    envp added

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,10 +1,11 @@
 #include "../incs/minishell.h"
 #include "../libs/ft_printf/includes/ft_printf.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
 	(void) argc;
 	(void) argv;
+	(void) envp;
 	ft_printf("Hello World!\n");
 	return (0);
 }
