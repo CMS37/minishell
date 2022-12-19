c01a4d8def1f19e8d298c387f09a9355ddb61dac
Younganswer <dudtj1999@gmail.com>
2022-12-19 16:28:15 +0900

    .

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,11 +1,26 @@
 #include "../incs/minishell.h"
 #include "../libs/ft_printf/includes/ft_printf.h"
+#include "../libs/get_next_line/includes/get_next_line.h"
+#include <unistd.h>
 
 int	main(int argc, char **argv, char **envp)
 {
+	char	*cmd;
+
+	while (TRUE)
+	{
+		ft_putstr_fd("minishell-1.0$ ", STDOUT_FILENO);
+		cmd = get_next_line(STDIN_FILENO);
+		if (cmd == NULL)
+			exit(EXIT_FAILURE);
+		if (ft_strncmp(cmd, "exit", 4) == 0)
+			break ;
+		free(cmd);
+	}
+	free(cmd);
+	
 	(void) argc;
 	(void) argv;
 	(void) envp;
-	ft_printf("minishell-1.0$ ");
 	return (0);
 }
