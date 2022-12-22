b9e0b82d50c9dbe11a2cd9c1223b2c89e2f20d8a
CMS37 <rhtn037@naver.com>
2022-12-22 23:11:34 +0900

    .

--- srcs/parser/syntax_err.c
+++ srcs/parser/syntax_err.c
@@ -2,8 +2,26 @@
 #define SYNTAX_ERR 1
 #define QUOTES_ERR 2
 
-/*
-int	syntax_err(void)
+t_bool	check_syntax_err(void)
+{
+	t_list	*tmp;
+
+	tmp = g_var->token_list;
+	if (!check_quotes(tmp))
+	{
+		printf("TESTMESSAGE \"QUOTES_NOT_PAIR\"\n");
+		g_var->exit_status = 2;
+		return (TRUE);
+	}
+	if (!check_syntax())
+	{
+		g_var->exit_status = 3;
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+t_bool	check_syntax(void)
 {
 	t_token	*token;
 	t_token	*prev;
@@ -21,33 +39,45 @@ int	syntax_err(void)
 		else
 			next = (t_token *) tmp->content;
 		if (token->type == T_PIPE && (next->type == T_PIPE || \
-			prev == NULL || )
-			(prev == NULL || next == NULL || find_path(prev->value) || \
-			next->type == T_PIPE))
-			return (1);
-		
+			prev == NULL || next == NULL || prev->type != T_WORD))
+			{
+				printf("SYNTAX ERR : PIPE\n");
+				return (FALSE);
+			}
 		if (token->type >= T_REDIRECT && \
-			(next != NULL && next->type != T_ARGV))
+			(next == NULL || next->type == PIPE))
 			token->type = 1;
-		
+			{
+				printf("SYNTAX ERR : NEWLINE\n");
+				return (FALSE);
+			}
 		prev = token;
 	}
 	return (0);
 }
-*/
 
-t_bool	check_syntax_err(void)
+int	parsing_process(char *str, t_cmd **cmd, int *i)
 {
-	t_list	*tmp;
-
-	tmp = g_var->token_list;
-	if (!check_quotes(tmp))
+	if (str[*i] == '\"' || str[*i] == '\'')
 	{
-		printf("TESTMESSAGE \"QUOTES_NOT_PAIR\"\n");
-		g_var->exit_status = 2;
-		return (TRUE);
+		if (parsing_quotes(str, i, cmd))
+		{
+			free(str);
+			return (1);
+		}
 	}
-	return (FALSE);
-//	if (!syntax_err())
-//		return (SYNTAX_ERR);
+	else if (str[*i] == ';' || str[*i] == '\\')
+		return (1);
+	else if (str[*i] == '<' || str[*i] == '>' || \
+							str[*i] == '|' || str[*i] == '$')
+	{
+		if (operation_word(cmd, str, i))
+		{
+			free(str);
+			return (1);
+		}
+	}
+	else
+		argument_word(cmd, str, i);
+	return (0);
 }
