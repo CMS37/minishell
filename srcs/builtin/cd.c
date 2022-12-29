e2574eeedec94d4a8ee0b3584d45c8947c540552
Younganswer <dudtj1999@gmail.com>
2022-12-29 18:48:47 +0900

    .

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -5,6 +5,7 @@
 int				builtin_cd(t_list *token_list, int fd);
 static char		*get_home(void);
 static t_bool	free_paths(char *home, char *path);
+static int		chdir_to_home(char *home);
 
 int	builtin_cd(t_list *token_list, int fd)
 {
@@ -14,17 +15,16 @@ int	builtin_cd(t_list *token_list, int fd)
 
 	(void) fd;
 	home = get_home();
-	if (token_list->next == NULL &&
-		(home == NULL || chdir(home) != 0) &&
-		free_paths(home, NULL))
-		return(print_err(errno, "cd", NULL, strerror(errno)));
+	if (ft_lstsize(token_list) == 1)
+		return (chdir_to_home(home));
 	arg = token_list->next->content;
 	if (arg->value[0] == '~')
 		path = ft_strjoin(home, arg->value + 1);
 	else
 		path = ft_strdup(arg->value);
-	if (chdir(path) != 0 && free_paths(home, path))
+	if ((path == NULL || chdir(path) != 0) && free_paths(home, path))
 		return (print_err(errno, "cd", arg->value, strerror(errno)));
+	free_paths(home, path);
 	return (g_var->exit_status);
 }
 
@@ -56,4 +56,12 @@ static t_bool	free_paths(char *home, char *path)
 	if (path)
 		free(path);
 	return (TRUE);
+}
+
+static int	chdir_to_home(char *home)
+{
+	if ((home == NULL || chdir(home) != 0) && free_paths(home, NULL))
+		return(print_err(errno, "cd", NULL, strerror(errno)));
+	free_paths(home, NULL);
+	return (g_var->exit_status);
 }
