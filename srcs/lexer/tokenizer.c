70224681ca1906fb8a05cc011abaeb9a769fe9e8
Younganswer <dudtj1999@gmail.com>
2022-12-30 21:38:08 +0900

    .

--- srcs/lexer/tokenizer.c
+++ srcs/lexer/tokenizer.c
@@ -4,23 +4,24 @@
 
 t_bool			tokenize(const char *line);
 t_bool			is_not_word(int c);
-static t_bool	handle_tokens(t_token **token, const char **line);
+static t_bool	handle_tokens(t_token **token, const char **line, t_bool *flag);
 
 t_bool	tokenize(const char *line)
 {
+	t_bool	quote_exists;
 	t_token	*cur;
 
+	quote_exists = FALSE;
 	cur = init_token();
 	while (*line)
 	{
 		if (is_not_word(*line))
-			handle_tokens(&cur, &line);
+			handle_tokens(&cur, &line, &quote_exists);
 		else
 			ft_strncat(&cur->value, line, 1);
 		line++;
 	}
-	if (cur->value[0] != '\0' ||
-		(cur->value[0] == '\0' && is_quote(*(line - 1))))
+	if (cur->value[0] != '\0' || quote_exists == TRUE)
 		ft_lstadd_back(&g_var->token_list, ft_lstnew(cur));
 	else
 		del_token(cur);
@@ -37,18 +38,23 @@ t_bool	is_not_word(int c)
 	);
 }
 
-static t_bool	handle_tokens(t_token **token, const char **line)
+static t_bool	handle_tokens(t_token **token, const char **line, t_bool *flag)
 {
-	if ((is_ifs(**line) || is_meta(**line)))
+	if ((is_ifs(**line) || is_meta(**line)) &&
+		((*token)->value[0] != '\0' || *flag == TRUE))
 	{
-		ft_lstadd_back(&g_var->token_list, ft_lstnew((*token)));
-		(*token) = init_token();
+		ft_lstadd_back(&g_var->token_list, ft_lstnew(*token));
+		*token = init_token();
+		*flag = FALSE;
 	}
 	if (is_meta(**line))
 		handle_meta(line);
 	else if (is_quote(**line))
-		handle_quote((*token), line);
+	{
+		handle_quote(*token, line);
+		*flag = TRUE;
+	}
 	else if (is_expand(**line))
-		handle_expand((*token), line);
+		handle_expand(*token, line);
 	return (TRUE);
 }
