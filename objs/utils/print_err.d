9af3174fbe10dd1cf34739ddea042124811651b8
CMS37 <rhtn037@naver.com>
2023-01-04 18:43:49 +0900

    .

--- objs/utils/print_err.d
+++
@@ -1,5 +0,0 @@
-objs/utils/print_err.o: srcs/utils/print_err.c \
-  srcs/utils/../../incs/subsystem.h incs/../libs/libft/incs/libft.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/stddef.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/__stddef_max_align_t.h \
-  srcs/utils/../../incs/builtin.h
